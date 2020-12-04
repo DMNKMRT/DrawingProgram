570f892bf425de1679ffc34534e53c04427dcd8a
acx593 <dominik.martin@haw-hamburg.de>
2020-12-04 08:36:19 +0100

    Hallo

---
+++ Einführung.py
@@ -0,0 +1,5 @@
+#Dies ist die Einführungsklasse
+
+class Einführung:
+    einführung = "Dies ist ein story board spiel. Beginne mit einer Tasteneingabe"
+
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # Dies ist ein kleines Story Board spiel
 # Made by Dominik Martin (acx593)
 
+import Einführung
+
+einf = Einführung.new()
+print(einf)
